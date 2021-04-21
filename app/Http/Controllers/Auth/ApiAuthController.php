2d4dc0abe93cf98bc05f0bc5421862ed382ced84
muthuajohn <caveomuthama@gmail.com>
2021-04-21 14:42:46 +0300

    feat:Added login functionality

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -29,4 +29,29 @@ class ApiAuthController extends Controller
         $response = ['token' => $token];
         return response($response, 200);
     }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:4|confirmed',
+        ]);
+        if ($validator->fails()) {
+            return response(['errors' => $validator->errors()->all()], 422);
+        }
+        $user = User::where('email', $request->email)->first();
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+                $response = ['token' => $token];
+                return response($response, 200);
+            } else {
+                $reponse = ["message" => "password mismatch"];
+                return response($response, 422);
+            }
+        } else {
+            $response = ["message" => "User does not exist"];
+            return response($response, 422);
+        }
+    }
 }
